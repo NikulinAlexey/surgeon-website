5b989be03ad622fce2f29be2256c91cea93db240
Alsekey <donaldduckisnotdead@gmail.com>
2025-11-15 23:52:10 +0300

    Правит FAQ, добавляет страницу 404, 500, выносит input  с label  в один компонент, добавляет в input кнопку reset

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   sassOptions: {
     includePaths: ["./src/scss"],
   },
