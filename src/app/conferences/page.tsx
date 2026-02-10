4b6cd69911e70c67edc6bdc02163c175532eb4a7
Alsekey <donaldduckisnotdead@gmail.com>
2026-02-10 13:15:16 +0300

    Удаляет ненужный импорт

--- src/app/conferences/page.tsx
+++ src/app/conferences/page.tsx
@@ -2,9 +2,6 @@ import Footer from "@/components/Footer";
 import { Header } from "@/components/header/Header";
 import Section from "@/components/Section";
 import SvgIcon from "@/components/ui/SvgIcon";
-import UniversalVideoEmbed, {
-  VideoPlayer,
-} from "@/components/universalVideoEmbed/UniversalVideoEmbed";
 import Link from "next/link";
 
 export default function ConferencesPage() {
